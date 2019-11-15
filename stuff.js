31b0e434cfadf4e081eb0b0ec8e549a1152c5dd2
Joe G <20873239+joe27g@users.noreply.github.com>
2019-11-15 00:53:25 -0600

    bugfixes + add error pages

--- stuff.js
+++ stuff.js
@@ -1,18 +1,3 @@
-const button = document.getElementById('nmenu-button');
-const menu = document.getElementById('nmenu');
-
-function toggleClass(elem, cName) {
-	if (!elem || !elem.className || !cName) return;
-	if (elem.className.indexOf(cName) === -1)
-		elem.className += ' ' + cName;
-	else
-		elem.className = elem.className.replace(cName, '').trim();
-}
-if (button) button.onclick = function() {
-	toggleClass(this, 'is-active');
-	toggleClass(menu, 'is-active');
-}
-
 var vue = new Vue({
 	el: '#body',
 	data: {
